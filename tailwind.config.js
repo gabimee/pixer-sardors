0c19b9244736b6fda4ca272f3e6d73e894862a11
sardor <frontendmii@gmail.com>
2024-03-18 22:11:15 +0500

    added css index.html pacage.json tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+      colors: {
+        white: '#fff',
+        body: '#fff',
+        bg:'#1AAB7B',
+        black: '#101010',
+        gray: '#636F87',
+        'dark-blue': '#2B3844',
+        'body-dark': '#202C36',
+      },
+    },
   },
   plugins: [],
 }
